e5dc4edabcca4c7f1ca72366a10e53705d5788c3
olavea <ola@olavea.com>
2025-07-17 07:26:06 +0200

    Will add validation later

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 
-
+// Create Ahoy, in ep (16)
 Route::get('gateways/create', function () {
+    // request()->validate([
+    //     'name' => ['required', 'min:3'],
+        
+    // ]); later
     return view('gateways.create');
 });
+
+
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
