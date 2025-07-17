a22c261b7fa5ff6d6f4b1bdbd3f0d52a7f8c5f4b
olavea <ola@olavea.com>
2025-07-17 07:21:37 +0200

    Added a form to create an Omvei Gateway and a route for that view

---
+++ resources/views/gateways/create.blade.php
@@ -0,0 +1,39 @@
+
+    <!-- 15:43 @csrf  -->
+    <form method="POST" action="/gateways">
+    @csrf 
+
+    <div>
+        <div>
+        <h2>Name a New Omvei Gateway ⛵</h2>
+        <p>We just need a name from you to start.</p>
+
+            <div>
+                <div>
+                    <label for="title">Name</label>
+                    <div>
+                        <div>
+                            <input 
+                                type="text" 
+                                name="name" 
+                                id="name" 
+                                placeholder="🥔 Potet på 🛴  ">
+                        </div>
+
+                    </div>
+            
+                
+            </div>
+            
+        </div>
+        </div>
+
+
+
+    <div>
+        <button type="button">Cancel</button>
+        <button type="submit">Save</button>
+    </div>
+    </form>
+
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+Route::get('gateways/create', function () {
+    return view('gateways.create');
+});
+
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
